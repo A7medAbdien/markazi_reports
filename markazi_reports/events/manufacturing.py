67845d89cf6911dcb0bad52e966bd2b4e1b00b38
A7medAbdien <ahmed.g.abdien@gmail.com>
2024-12-12 19:18:56 +0300

    add some commments

--- markazi_reports/events/manufacturing.py
+++ markazi_reports/events/manufacturing.py
@@ -7,16 +7,13 @@ def on_validate_work_order(doc, _):
 
     # consider only allowed companies
     markazi_settings = frappe.get_doc("Markazi Settings")
-    # print(f"\n\n\n {markazi_settings.as_dict()} \n\n\n")
-
     allowed_companies = [
         company.company for company in markazi_settings.allowed_companies]
-    # print(f"\n\n\n {allowed_companies[0].as_dict()} \n\n\n")
-
     if (doc.company not in allowed_companies):
         return
     # print(f"\n\n\n {doc.company} \n\n\n")
 
+    # set nessessary values and submit Work Order
     frappe.db.set_value("Work Order", doc.name, "skip_transfer", 1)
     frappe.db.set_value("Work Order", doc.name, "allow_alternative_item", 1)
     frappe.db.set_value("Work Order", doc.name, "docstatus", 1)
@@ -31,12 +28,13 @@ def on_validate_work_order(doc, _):
     ))
     # print(f"\n\n\n {items_out_of_stock} \n\n\n")
 
+    # make stock entry and save it
     stock_entry = make_stock_entry(doc.name, "Manufacture", doc.qty)
     stock_entry = frappe.get_doc(stock_entry)
     stock_entry.save()
     # print(f"\n\n\n stock_entry {stock_entry} \n\n\n")
 
-    # case A: if no out of stock
+    # submit stock entry if no items out of stock
     if (len(items_out_of_stock) == 0):
         stock_entry.submit()
 
